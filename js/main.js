36d6fe72573f36d39ea3a8f59a1edd64778a9afb
Vishnu Solanki Raika <108605628+VishnuRaika04@users.noreply.github.com>
2024-05-14 15:11:08 +0530

    flies uploaded

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyA5HjUaliOz6IzLLB8vdjbSAo4ok6FHQRw",
+    authDomain: "propertydekho-69315.firebaseapp.com",
+    databaseURL: "https://propertydekho-69315-default-rtdb.firebaseio.com",
+    projectId: "propertydekho-69315",
+    storageBucket: "propertydekho-69315.appspot.com",
+    messagingSenderId: "571023693748",
+    appId: "1:571023693748:web:a249fa32ae6fa6a33a44e1",
+    measurementId: "G-99ZB86NLEQ"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
